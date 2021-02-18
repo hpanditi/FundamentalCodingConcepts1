0c2ecccec1d485cd655b98c4958eface23c8d0af
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-02-18 17:52:33 -0500

    Added array programs.

---
+++ ArrayPractice2.java
@@ -0,0 +1,8 @@
+// This program was created on February 18, 2021
+
+public class ArrayPractice2 {
+    public static void main(String[] args) {
+        String[] cars = {"Honda", "Toyota", "Ford"};
+        System.out.println(cars[0]);
+    }
+}
