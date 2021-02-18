cb3b336d29794e8d6be6ccdc3af952d019e2901f
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-02-18 16:12:07 -0500

    Add existing file

---
+++ HelloEarth.java
@@ -0,0 +1,7 @@
+// This program was created on February 18, 2021
+
+public class HelloEarth {
+    public static void main(String[] args) {
+        System.out.println("Hello Earth and its citizens!");
+    }
+}
