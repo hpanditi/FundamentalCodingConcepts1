80dd88919519b713811483f6242acfc6af389e15
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-02-18 17:06:51 -0500

    Added a HelloStudents program

---
+++ HelloStudents.java
@@ -0,0 +1,7 @@
+// This program was created on February 18, 2021
+
+public class HelloStudents {
+    public static void main(String[] args) {
+        System.out.println("Hello Students! Thank you for coming to my class.");
+    }
+}
