2b1b792848aabe264a27856b9ae981d559da8377
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-02-18 16:22:16 -0500

    Added a sentence

--- HelloEarth.java
+++ HelloEarth.java
@@ -3,5 +3,6 @@
 public class HelloEarth {
     public static void main(String[] args) {
         System.out.println("Hello Earth and its citizens! Greetings to all of you!");
+        System.out.println("Earth is a beautiful planet");
     }
 }
