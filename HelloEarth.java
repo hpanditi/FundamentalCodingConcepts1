9d74cb7d899cd1376a4ac8ec20c72573f3dec34c
hpanditi <59629153+hpanditi@users.noreply.github.com>
2021-02-18 16:19:48 -0500

    Added a sentence

--- HelloEarth.java
+++ HelloEarth.java
@@ -2,6 +2,6 @@
 
 public class HelloEarth {
     public static void main(String[] args) {
-        System.out.println("Hello Earth and its citizens!");
+        System.out.println("Hello Earth and its citizens! Greetings to all of you!");
     }
 }
