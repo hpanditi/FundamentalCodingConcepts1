9cf3455d34609b20a83a6f082b7c3e7c2d49ff46
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-02-19 16:21:57 -0500

    Added line break practice programs.

---
+++ LineBreakPractice.java
@@ -0,0 +1,11 @@
+// This program was created on February 19, 2021
+
+public class LineBreakPractice {
+    public static void main(String[] args) {
+        String lineone = "Jim went to the store";
+        String linetwo = "He bought a gallon of milk";
+        String linethree = "He also bought a loaf of bread";
+        String taskone = lineone + "\n" + linetwo + "\n" + linethree;
+        System.out.println(taskone);
+    }
+}
---
+++ LineBreakPractice1.java
@@ -0,0 +1,12 @@
+// This program was created on February 19, 2021
+
+public class LineBreakPractice1 {
+    public static void main(String[] args) {
+        String firstsentence = "List of new employees: ";
+        String secondsentence = "Jim";
+        String thirdsentence = "Michael";
+        String fourthsentence = "Steve";
+        String statementone = firstsentence + "\n" + secondsentence + "\n" + thirdsentence + "\n" + fourthsentence;
+        System.out.println(statementone);
+    }
+}
