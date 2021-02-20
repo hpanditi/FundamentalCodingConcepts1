42e5bd25de9069b7f3100ea7b748b237408948e1
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-02-19 20:21:26 -0500

    Adjusted a word

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println ("Hello World! This is the first program I want to contribute to my Fundamental Coding Concepts repository");
+        System.out.println ("Hello World! This is the first program I want to contribute to my Fundamental Coding Concepts Repository");
     }
 }
