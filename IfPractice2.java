6c5e3c628921789605ab30929b8db36d1b79809d
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-02-21 17:58:59 -0500

    Added an ifpractice program

---
+++ IfPractice2.java
@@ -0,0 +1,17 @@
+// This program was created on February 21, 2021
+
+public class IfPractice2 {
+    public static void main(String[] args) {
+        int a = 101;
+
+        int b = 100;
+
+        if (a > b) {
+            System.out.println("Yes that's true");
+        }
+        else {
+            System.out.println("No that's false");
+            }
+        }
+    }
+
