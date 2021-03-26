b72cb60ab5986b4fd7fa264fffde13444809d54e
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-03-25 21:47:42 -0400

    Added program about String

---
+++ StringPractice1.java
@@ -0,0 +1,10 @@
+// This program was created on March 25, 2021
+
+public class StringPractice1 {
+    public static void main(String[] args) {
+
+        String str = "abc";
+
+        System.out.println(str);
+    }
+}
